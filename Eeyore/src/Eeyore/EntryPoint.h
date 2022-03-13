d955aa5a404b9890db53bfd53a101b45d56392eb
Rosy Fu <rfu@naughtydog.com>
2022-03-12 20:51:06 -0800

    add logging system, define macros.

--- Eeyore/src/Eeyore/EntryPoint.h
+++ Eeyore/src/Eeyore/EntryPoint.h
@@ -3,6 +3,11 @@
 extern Eeyore::Application* Eeyore::CreateApplication();
 
 int main(int argc, char**argv) {
+	Eeyore::Log::Init();
+	ER_CORE_WARN("Initialized Log!");
+	int a=5;
+	ER_INFO("Hello! Var={0}", a);
+
 	auto app = Eeyore::CreateApplication();
 	app->Run();
 	delete app;
--- Eeyore/src/Eeyore/Log.h
+++ Eeyore/src/Eeyore/Log.h
@@ -8,9 +8,25 @@ namespace Eeyore {
 		static void Init();
 
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
-		inline static std::shared_ptr<spdlog::logger>& GetClintLogger() { return s_ClientLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
 	private:
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
 	};
 }
+//core log macros
+#define ER_CORE_TRACE(...)		::Eeyore::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define ER_CORE_INFO(...)		::Eeyore::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define ER_CORE_WARN(...)		::Eeyore::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define ER_CORE_ERROR(...)		::Eeyore::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define ER_CORE_FATAL(...)		::Eeyore::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+//client log macros
+#define ER_TRACE(...)			::Eeyore::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define ER_INFO(...)			::Eeyore::Log::GetClientLogger()->info(__VA_ARGS__)
+#define ER_WARN(...)			::Eeyore::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define ER_ERROR(...)			::Eeyore::Log::GetClientLogger()->error(__VA_ARGS__)
+#define ER_FATAL(...)			::Eeyore::Log::GetClientLogger()->fatal(__VA_ARGS__)
+
+//if distribution build
+//#define ER_INFO //so to not include in dist build
